d9b10522ceee32068fc091f2b780264ee5c022b8
leandreAlly <tuyambazeleandre@gmail.com>
2023-01-11 13:17:58 +0200

    activate humburger to admin page

--- ui/js/blog.js
+++ ui/js/blog.js
@@ -6,10 +6,10 @@ let val;
 window.onscroll = function() {
   if(document.documentElement.scrollTop > 20){
     nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
+    // scrollBtn.style.display = "block";
   }else{
     nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
+    // scrollBtn.style.display = "none";
   }
 
 }
@@ -24,19 +24,19 @@ menuBtn.onclick = function(){
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
   body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
+//   scrollBtn.style.pointerEvents = "none";
 }
 cancelBtn.onclick = function(){
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
   menuBtn.style.pointerEvents = "auto";
   body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
+//   scrollBtn.style.pointerEvents = "auto";
 }
 
 // Side Navigation Bar Close While We Click On Navigation Links
 let navLinks = document.querySelectorAll(".menu li a");
-for (let i = 0; i < navLinks.length; i++) {
+for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click" , function() {
     navBar.classList.remove("active");
     menuBtn.style.opacity = "1";
